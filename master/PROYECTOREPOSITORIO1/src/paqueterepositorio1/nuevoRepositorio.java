79754c4910315a73998172f52fa41582366244b9
johanndaniellopezguerrero <johanndaniellopezguerrero@localhost>
2026-02-13 13:05:58 +0100

    Commit desde la rama1

--- master/PROYECTOREPOSITORIO1/src/paqueterepositorio1/nuevoRepositorio.java
+++ master/PROYECTOREPOSITORIO1/src/paqueterepositorio1/nuevoRepositorio.java
@@ -1,5 +1,9 @@
 package paqueterepositorio1;
 
 public class nuevoRepositorio {
+	private String name = "Mob1";
+	private final int LIFE = 20;
+	private int attack = 30;
+	private int attackSpeed = 2;
 
 }
