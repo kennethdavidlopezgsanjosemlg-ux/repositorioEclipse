84b407c3ad7530bd659e3519d0e41a013f54a172
emoyanocar <emoyano@fundacionloyola.es>
2026-02-20 12:55:01 +0100

    Problemas solucionados

--- master/PROYECTOREPOSITORIO1/src/paqueterepositorio1/claserepositorio1.java
+++ master/PROYECTOREPOSITORIO1/src/paqueterepositorio1/claserepositorio1.java
@@ -1,5 +1,5 @@
 package paqueterepositorio1;
 
 public class claserepositorio1 {
- //Todavía puedes aprobar la evaluación
+ //Todavía puedes aprobar la evaluación 
 }
--- master/PROYECTOREPOSITORIO1/src/paqueterepositorio1/nuevoRepositorio.java
+++ master/PROYECTOREPOSITORIO1/src/paqueterepositorio1/nuevoRepositorio.java
@@ -1,9 +1,9 @@
 package paqueterepositorio1;
 
 public class nuevoRepositorio {
-	private int numero =10;
+	private int numero = 10;
 
 	public nuevoRepositorio() {
-		
+	}
 
 }
